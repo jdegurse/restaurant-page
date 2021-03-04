43c7329d0bf9a306b91676ac6fb313a57b7bf907
James DeGurse <jamesdegurse@gmail.com>
2021-03-04 16:17:47 -0500

    Add nav menu functionality
    Clicking each nav button will dump the main innerHTML and load in the
    innerHTML for that page. Will also highlight which page the user is on.

--- src/index.js
+++ src/index.js
@@ -1,5 +1,2 @@
 import { pageLoad } from './pageload.js';
-
-console.log("This is working.")
-
 pageLoad();
--- src/pageload.js
+++ src/pageload.js
@@ -1,4 +1,19 @@
 function pageLoad() {
+    ////////    INNER HTML CONTENT    ////////
+    const home_content = `
+    
+    `;
+
+    const menu_content = `
+    
+    `;
+
+    const contact_content = `
+    
+    `;
+
+
+
     ////////    CREATE HEADER    ////////
     const header = document.createElement('div');
     header.setAttribute('id', 'header');
@@ -34,18 +49,52 @@ function pageLoad() {
     ////////    CREATE MAIN    ////////
     const main = document.createElement('div');
     main.setAttribute('id', 'main');
-    main.innerHTML = `
+
+    ////////    CLEAR MAIN INNER HTML    ////////
+    function clearMain() {
+        main.innerHTML = '';
+    }
+
+
+
+    ////////    NAVIGATION CLICK FUNCTIONS    ////////
+    function openHome() {
+        nav_home.classList.add('active');
+        nav_menu.classList.remove('active');
+        nav_contact.classList.remove('active');
+        clearMain();
+        main.innerHTML = `
         <p>
-        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do 
-        eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut 
-        enim ad minim veniam, quis nostrud exercitation ullamco laboris 
-        nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor 
-        in reprehenderit in voluptate velit esse cillum dolore eu 
-        fugiat nulla pariatur. Excepteur sint occaecat cupidatat non 
-        proident, sunt in culpa qui officia deserunt mollit anim id est 
-        laborum.
+        HOME
         </p>
-        `;
+        `
+    }
+
+    nav_home.addEventListener('click', openHome);
+
+    nav_menu.addEventListener('click', function openMenu() {
+        nav_home.classList.remove('active');
+        nav_menu.classList.add('active');
+        nav_contact.classList.remove('active');
+        clearMain();
+        main.innerHTML = `
+        <p>
+        MENU
+        </p>
+        `
+    });
+
+    nav_contact.addEventListener('click', function openContact() {
+        nav_home.classList.remove('active');
+        nav_menu.classList.remove('active');
+        nav_contact.classList.add('active');
+        clearMain();
+        main.innerHTML = `
+        <p>
+        Contact Us!
+        </p>
+        `
+    });
 
 
 
@@ -69,6 +118,8 @@ function pageLoad() {
     content.appendChild(nav);
     content.appendChild(main);
     content.appendChild(footer);
+
+    openHome();
 }
 
 export { pageLoad };
